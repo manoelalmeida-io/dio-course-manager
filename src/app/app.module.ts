d14dae74b70676855699f2562644836271eb7a77
Manoel Almeida <manoel.almeida@bandtec.com.br>
2020-03-20 11:34:16 -0300

    Segregando em componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,25 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
 import { CourseService } from './courses/course.service';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { StarComponent } from './star/star.component';
-import { Error404Component } from './error-404/error-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/component/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CourseListComponent,
-    StarComponent,
-    NavBarComponent,
-    CourseInfoComponent,
-    Error404Component,
-    ReplacePipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
-      {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
     ])
   ],
